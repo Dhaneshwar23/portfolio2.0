213075893e97352fb170b6a582dc41846efafd2e
Dhaneshwar23 <dhaneshwarkoshti@gmail.com>
2024-05-09 13:36:40 -0400

    clean up

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -38,38 +38,7 @@ type Props = {
 
 
 export default function Home({  }) {
-  // const [data, setData] = useState<Props | null>(null
-  // );
-
-  // const processData = async () => {
-  //   try {
-  //     const pageInfo: PageInfo = await fetchPageInfo();
-  //     const experiences: Experience[] = await fetchExperiences();
-
-  //     const skills: Skill[] = await fetchSkills();
-
-  //     const projects: Project[] = await fetchProjects();
-
-  //     const socials: Social[] = await fetchSocials();
-  //     // Example processing of data
-  //     const processedData = {
-  //       pageInfo,
-  //       experiences,
-  //       skills,
-  //       projects,
-  //       socials
-  //     };
-  //     setData(processedData);
-  //   }
-  //   catch (err) {
-  //     console.log('err happend in page.tsx-->  ', err);
-  //   }
-  // }
-  // useEffect(() => {
-  //   // Define a function to process data
-  //   processData();
-  // }, []);
-
+  
   const { data, loading } = apiData();
 
   return (
